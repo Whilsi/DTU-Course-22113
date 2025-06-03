7d975d884e46ac1a46d49049901716c870411896
William <william@destricker.dk>
2025-06-03 22:27:32 +0200

    Completed week3 exercise 2.

---
+++ Week3/ex2.py
@@ -0,0 +1,22 @@
+import re,sys
+
+infile = open(sys.argv[1])
+
+sequences = ''
+
+for line in infile:
+    if line.startswith('>'):
+        continue
+
+    sequences += ''.join(line.split())
+
+print(sequences)
+
+DNA_result = re.search(r'[ATCG]+',sequences)
+Prot_result = re.search(r'[GAVLITSMCPFYWHKRDENQ]+',sequences)
+if DNA_result is not None:
+    print('DNA fasta')
+elif Prot_result is not None:
+    print('Protein fasta')
+else:
+    print('Not fasta')
