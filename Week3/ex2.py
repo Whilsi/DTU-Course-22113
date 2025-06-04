7fe12747525e04af20068ec0ff8eabaa7009b1e5
Whilsi <william@destricker.dk>
2025-06-04 16:53:20 +0200

    Added tests for proteins in week 3 ex2 and fixed some bugs using a dictionary that counts all letters.

--- Week3/ex2.py
+++ Week3/ex2.py
@@ -10,11 +10,20 @@ for line in infile:
 
     sequences += ''.join(line.split())
 
-DNA_result = re.search(r'[ATCG]+',sequences)
-Prot_result = re.search(r'[GAVLITSMCPFYWHKRDENQ]+',sequences)
+DNA_result = re.search(r'^([ATCG]+)$',sequences)
+Prot_result = re.search(r'^([GAVLITSMCPFYWHKRDENQ]+)$',sequences)
 if DNA_result is not None:
     print('DNA fasta')
 elif Prot_result is not None:
     print('Protein fasta')
 else:
     print('Not fasta')
+
+letter_dict = {}
+for letter in sequences:
+    if letter in letter_dict:
+        letter_dict[letter] += 1
+    else:
+        letter_dict[letter] = 1
+
+print(letter_dict)
