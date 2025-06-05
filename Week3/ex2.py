37889ca9a278c1962d9597a9659793ee72391b14
Whilsi <william@destricker.dk>
2025-06-05 12:26:29 +0200

    Removed the letter dictionary used for debugging in week 3 ex2.

--- Week3/ex2.py
+++ Week3/ex2.py
@@ -18,12 +18,3 @@ elif Prot_result is not None:
     print('Protein fasta')
 else:
     print('Not fasta')
-
-letter_dict = {}
-for letter in sequences:
-    if letter in letter_dict:
-        letter_dict[letter] += 1
-    else:
-        letter_dict[letter] = 1
-
-print(letter_dict)
