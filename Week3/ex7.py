1b01af807e9abaf4c6dda4764ef8c7617031da23
Whilsi <william@destricker.dk>
2025-06-05 14:47:02 +0200

    Completed week 3 ex7.

---
+++ Week3/ex7.py
@@ -0,0 +1,23 @@
+import sys,re
+
+infile = open(sys.argv[1])
+
+flag = False
+aminoacid_sequence = ''
+
+for line in infile:
+    translation_match = re.search('/translation="',line)
+    if translation_match is not None:
+        flag = True
+        if '\n' not in line.split('"')[1]:
+            flag = False
+            aminoacid_sequence = line.split('"')[1]
+        else:
+            aminoacid_sequence += line.strip().split('"')[1]
+    elif flag and '"' not in line:
+        aminoacid_sequence += line.strip()
+    elif flag and '"' in line:
+        flag = False
+        aminoacid_sequence += line.strip().split('"')[0]
+        
+print(aminoacid_sequence)
