42f0b4ce3896ba8ea32975ff398efb0551688b38
Whilsi <william@destricker.dk>
2025-06-05 14:07:16 +0200

    Completed week 3 ex5

--- Week3/ex5.py
+++ Week3/ex5.py
@@ -1,2 +1,18 @@
-import re
+import re,sys
 
+infile = open(sys.argv[1])
+
+for line in infile:
+    ORG_match = re.search(r'ORGANISM.+',line)
+    DEF_match = re.search(r'DEFINITION.+',line)
+    ACC_match = re.search(r'ACCESSION.+',line)
+    if ORG_match is not None:
+        organism = ' '.join(line.split()[1:])
+    elif DEF_match is not None:
+        definition = ' '.join(line.split()[1:])
+    elif ACC_match is not None:
+        accession = ' '.join(line.split()[1:])
+
+print('Accession number:',accession)
+print('Definition:',definition)
+print('Organism:',organism)
