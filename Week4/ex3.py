e50a0101738d818f99c9cda4367a1cec021e3de1
William <william@destricker.dk>
2025-06-05 21:19:51 +0200

    Completed week 4 ex3.

---
+++ Week4/ex3.py
@@ -0,0 +1,33 @@
+def fastaread(filename:str) -> list:
+    '''Takes a fasta file name as the input and returns a list with the headers and a list with the sequences'''
+
+    import re
+
+    header_list = []
+    sequence_list = []
+    sequence_str = ''
+
+    if isinstance(filename,str) and re.search(r'(.fsa)$',filename) is not None:
+        infile = open(filename)
+
+        for line in infile:
+            if line.startswith('>'):
+                if sequence_str is not '':
+                    sequence_list.append(''.join(sequence_str.split()))
+                    sequence_str = ''
+                header_list.append(line.strip())
+            else:
+                sequence_str += line
+
+        if sequence_str is not '':
+            sequence_list.append(''.join(sequence_str.split()))
+        
+        return header_list,sequence_list
+    else:
+        return None
+    
+result = fastaread('dna7.fsa')
+print(result[0])
+print(result[1])
+
+            
