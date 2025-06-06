cee42dc6e4ee7d093facd475137d82b68efb816a
Whilsi <william@destricker.dk>
2025-06-06 13:12:49 +0200

    Added function to week 5 ex2.

--- Week5/ex2.py
+++ Week5/ex2.py
@@ -6,14 +6,17 @@ for line in infile:
 
 infile.close()
 
-matrix_transposed = []
+def transpose(matrix:list) -> list:
+    matrix_transposed = []
+    for i in range(len(matrix[0])):
+        matrix_transposed += [[]]
+        for j in range(len(matrix)):
+            matrix_transposed[i] += [matrix[j][i]]
+    return matrix_transposed
 
-for i in range(len(matrix[0])):
-    matrix_transposed += [[]]
-    for j in range(len(matrix)):
-        matrix_transposed[i] += [matrix[j][i]]
-
-for row in matrix_transposed:
+matrix = transpose(matrix)
+    
+for row in matrix:
     for num in row:
         print(num,end='\t')
     print()
