c556b1dda3015601138d98117c3f8ac8a08eb6a2
Whilsi <william@destricker.dk>
2025-09-30 15:51:42 +1000

    Removed code used for debugging

--- Week9/np_exercise.py
+++ Week9/np_exercise.py
@@ -7,5 +7,5 @@ gene_expression1_norm = gene_expression1-gene_expression1.mean(axis=1).reshape(1
 gene_expression2_norm = gene_expression2-gene_expression2.mean(axis=1).reshape(10,1)
 
 normalized_array = np.hstack((gene_expression1_norm,gene_expression2_norm))
-print(normalized_array.shape)
+
 np.savetxt('normalized_array.npy',normalized_array,delimiter=',')
